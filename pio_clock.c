89c1acc1f11795398beabd7989f5364006fda7ee
Michal Kowalik <mkowalik@curriedpython.eu>
2023-11-27 23:43:36 +0100

    Generate clock signal

--- pio_clock.c
+++ pio_clock.c
@@ -2,11 +2,17 @@
 // Created by Michal Kowalik on 27.11.23.
 //
 #include <stdio.h>
-#include "pico/stdlib.h"
 #include "hardware/pio.h"
 
-#include "pi80.pio.h"
+#include "squarewave.pio.h"
+
+#define CLK_PIN 18     // clock pin
+#define CLK_DIVIDER 30 // clock divider = 120MHz / 30 = 4MHz
 
 void start_clock() {
     printf("starting the pio clock\r\n");
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &squarewave_program);
+    uint sm = pio_claim_unused_sm(pio, true);
+    squarewave_program_init(pio, sm, offset, CLK_DIVIDER, CLK_PIN);
 }
